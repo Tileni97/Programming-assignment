f5ab929718872b809f35a2fd8b773e5f2a1cbb6b
Tyan47 <114954698+Tyan47@users.noreply.github.com>
2022-10-10 22:15:20 +0100

    Update Cancel_Admin.java

--- Cancel_Admin.java
+++ Cancel_Admin.java
@@ -1,21 +1,32 @@
 import java.util.Scanner;
 
-public class Cancel_Admin {
-    public static void main(String[] args) {
-        int input;
-        String reasonOfcancelation;
-        Scanner Input = new Scanner(System.in);
-        int[] ordernum = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
-        System.out.println("Enter Order number");
-        input = Input.nextInt();
-        for (int i = 0; i < ordernum.length; i++) {
-            if (ordernum[20]!=input) {
-                System.out.println("order " + input + " canceled");
-                break;
-            }
+class LinearSearchAlgorithm {
+    static int LSA(int[] orderNUM, int length, int x)
+    {
+        for (int i = 0; i < length; i++) {
+            if (orderNUM[i] == x)
+                return i;
         }
-        System.out.println("Entre a reason for cancellation");
-        reasonOfcancelation= Input.nextLine();
-        System.out.println(reasonOfcancelation);
+        return -1;
+    }
+    public static void main(String[] args)
+    {
+        String Reason;
+        int[] ordrenum = { 0, 1, 2, 3, 4, 5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20 };
+        int length = ordrenum.length;
+        Scanner sc=new Scanner(System.in);
+        System.out.println(" Enter reason");
+        Reason=sc.nextLine();
+        System.out.println("Enter order number");
+        int x = sc.nextInt();
+        int index = LSA(ordrenum, length, x);
+        if (index == -1)
+            System.out.println("order not found ");
+        else
+            System.out.println("order " + index +" Canceled "+" Due to the following reason : " + Reason );
+
+
+
+
     }
 }
